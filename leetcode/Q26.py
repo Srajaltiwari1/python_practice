fc177584cb4f60625c4b9e68157be99d92e597a4
srajal tiwari <tiwarisrajal880@gmail.com>
2025-04-29 23:14:18 +0530

    Q_88

--- leetcode/Q26.py
+++ leetcode/Q26.py
@@ -2,13 +2,19 @@
 # Output: 2, nums = [1,2,_]
 # Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
 # It does not matter what you leave beyond the returned k (hence they are underscores).
+# nums=[1,1,2]
+# sets=set({})
+# for i in nums:
+#     sets.add(i)
+# k=len(sets)
+# output=[]
+# for i in sets:
+#     output.append(i)
+# print(len(output),",",(output))
 nums=[1,1,2]
-sets=set({})
-for i in nums:
-    sets.add(i)
-k=len(sets)
-output=[]
-for i in sets:
-    output.append(i)
-print(len(output),",",(output))
-
+k = 1  # index for placing next unique element
+for i in range(1, len(nums)):
+    if nums[i] != nums[i - 1]:
+        nums[k] = nums[i]
+        k += 1
+print(k)
