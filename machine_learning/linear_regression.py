fd844c36045e8cf2969a14b1f3820bb48d183552
srajal tiwari <tiwarisrajal880@gmail.com>
2025-05-11 18:08:38 +0530

    linear regression

--- machine_learning/linear_regression.py
+++ machine_learning/linear_regression.py
@@ -1,16 +1,12 @@
 import pandas as pd
 import seaborn as sns
 import numpy as np
+import openpyxl as excel
 from sklearn.linear_model import LinearRegression
-data= {
-    'salary_before':[5000,6000,7000,4000,5500,6500,4500,7500,5800,4800],
-    'salary_inc':[50000,60000,70000,40000,55000,65000,45000,75000,58000,48000],
-    'height':[175,160,180,165,170,168,178,162,172,166]
-}
-df=pd.DataFrame(data)
+
+# Load the Excel file into a DataFrame
+df = pd.read_excel(r"C:\Users\srajal\Desktop\vsTutorial\pythonPractice\machine_learning\tips.xlsx")
+ed = pd.read_excel(r"C:\Users\srajal\Desktop\vsTutorial\pythonPractice\machine_learning\weather_data.xlsx")
+# Display the DataFrame
 print(df)
-X=df[['salary_before']]
-y=df['salary_inc']
-model=LinearRegression()
-model.fit(X,y)
-model.predict[['salary_before']]
+print(ed)
